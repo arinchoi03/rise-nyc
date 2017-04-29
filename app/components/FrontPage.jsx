ed121c5b6e975651bfe375cf730ea637da3697e3
Yejee Arin Choi <yej.arin.choi@gmail.com>
2017-04-29 09:35:39 -0400

    changed up front page

--- app/components/FrontPage.jsx
+++ app/components/FrontPage.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'react-router'
 // navigator.getGeolocation?
 
-const FrontPage = props => {
-
-  return (
+const FrontPage = props => (
     <div>
-    Front Page tbd
+    <h1 className="title">RISE UP</h1>
+    <h2 className="title">NYC</h2>
+    <h4 className="title">Find help to get you to elevator access</h4>
+    <h4 className="title">Help someone else</h4>
     </div>
   )
-}
 
 export default FrontPage
